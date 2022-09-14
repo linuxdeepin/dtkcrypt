a8f49e4631d3be9dd4cd44eccb86f0dbc0f04dfa
guoyao <guoyao@uniontech.com>
2022-09-14 17:47:48 +0800

    feat: change demo to crypt
    change demo to crypt
    
    Log:

--- include/namespace.h
+++ include/namespace.h
@@ -2,9 +2,9 @@
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
-#define DDEMO_NAMESPACE Dtk::Demo
+#define DCRYPT_NAMESPACE Dtk::Crypt
 
-#define DDEMO_USE_NAMESPACE using namespace DDEMO_NAMESPACE;
+#define DCRYPT_USE_NAMESPACE using namespace DCRYPT_NAMESPACE;
 
-#define DDEMO_BEGIN_NAMESPACE namespace Dtk { namespace Demo {
-#define DDEMO_END_NAMESPACE }}
+#define DCRYPT_BEGIN_NAMESPACE namespace Dtk { namespace Crypt {
+#define DCRYPT_END_NAMESPACE }}
